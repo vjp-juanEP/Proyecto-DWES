36be6fdf57aed71aac5652e7d6a69f8b345eb59f
Juan Exposito Palomo <jexpositop05@informatica.iesvalledeljerteplasencia.es>
2024-10-16 16:27:47 +0200

    Parte 2

--- views/partials/nav.part.php
+++ views/partials/nav.part.php
@@ -16,16 +16,16 @@
     </div>
     <div class="collapse navbar-collapse navbar-right" id="menu">
       <ul class="nav navbar-nav">
-        <li class="<?php echo esOpcionMenuActiva('index.php') ? 'active' : ''; ?>">
+        <li class="lien <?php echo esOpcionMenuActiva('index.php') ? 'active' : ''; ?>">
           <a href="index.php"><i class="fa fa-home sr-icons"></i> Home</a>
         </li>
-        <li class="<?php echo esOpcionMenuActiva('about.php') ? 'active' : ''; ?>">
+        <li class="lien <?php echo esOpcionMenuActiva('about.php') ? 'active' : ''; ?>">
           <a href="about.php"><i class="fa fa-bookmark sr-icons"></i> About</a>
         </li>
-        <li class="<?php echo existeOpcionMenuActivaEnArray(['blog.php', 'single_post.php']) ? 'active' : ''; ?>">
+        <li class="lien <?php echo existeOpcionMenuActivaEnArray(['blog.php', 'single_post.php']) ? 'active' : ''; ?>">
           <a href="blog.php"><i class="fa fa-file-text sr-icons"></i> Blog</a>
         </li>
-        <li class="<?php echo esOpcionMenuActiva('contact.php') ? 'active' : ''; ?>">
+        <li class="lien <?php echo esOpcionMenuActiva('contact.php') ? 'active' : ''; ?>">
           <a href="contact.php"><i class="fa fa-phone-square sr-icons"></i> Contacto</a>
         </li>
       </ul>
