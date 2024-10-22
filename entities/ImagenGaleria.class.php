bc22dd6e23a10a49517d07a8566e8a415f368736
Juan Exposito Palomo <jexpositop05@informatica.iesvalledeljerteplasencia.es>
2024-10-22 20:57:44 +0200

    .

---
+++ entities/ImagenGaleria.class.php
@@ -0,0 +1,84 @@
+<?php
+class ImagenGaleria{
+    //Constantes
+    const RUTA_IMAGENES_PORTAFOLIOS = '/images/index/portfolio/';
+    const RUTA_IMAGENES_GALLERY = '/images/index/gallery/';
+    //Variables
+    private $nombre;
+    private $descripcion;
+    private $numVisualizaciones;
+    private $numLikes;
+    private $numDownloads;
+
+    //Constructor
+    public function __construct( $nombre,  $descripcion,  $numVisualizaciones = 0,  $numLikes = 0 ,  $numDownloads = 0)
+    {
+        $this->nombre = $nombre;
+        $this->descripcion = $descripcion;
+        $this->numVisualizaciones = $numVisualizaciones;
+        $this->numLikes = $numLikes;
+        $this->numDownloads = $numDownloads;
+    }
+
+    //Setters
+    public function setNombre( $nombre): void 
+    {
+        $this->nombre = $nombre;
+    }
+
+	public function setDescripcion( $descripcion): void 
+    {
+        $this->descripcion = $descripcion;
+    }
+
+	public function setNumVisualizaciones( $numVisualizaciones): void {
+        $this->numVisualizaciones = $numVisualizaciones;
+    }
+
+	public function setNumLikes( $numLikes): void 
+    {
+        $this->numLikes = $numLikes;
+    }
+
+	public function setNumDownloads( $numDownloads): void 
+    {
+        $this->numDownloads = $numDownloads;
+    }
+
+    //Getter
+	public function getNombre() : string
+    {
+        return $this->nombre;
+    }
+
+	public function getDescripcion() : string
+    {
+        return $this->descripcion;
+    }
+
+	public function getNumVisualizaciones() : int
+    {
+        return $this->numVisualizaciones;
+    }
+
+	public function getNumLikes() : int
+    {
+        return $this->numLikes;
+    }
+
+	public function getNumDownloads() : int
+    {
+        return $this->numDownloads;
+    }
+
+	public function getUrlPortafolio():string{
+        return self::RUTA_IMAGENES_PORTAFOLIOS.$this->getNombre();
+    }
+
+    public function getUrlGallery():string{
+        return self::RUTA_IMAGENES_GALLERY.$this->getNombre();
+    }
+
+	
+}
+?>
--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php 
+    require 'entities/ImagenGaleria.class.php';
+
+    $imagenGaleria = [];
+
+    for ($i = 0; $i < 12 ; $i++) { 
+        $imagen = new ImagenGaleria( ($i + 1) . '.jpg', 'descripcion imagen ' . ($i + 1) , rand(1,1000),rand(1,500),rand(1,100));
+        array_push($imagenGaleria,$imagen);
+    }
+
     require 'views/index.view.php';
 ?>
